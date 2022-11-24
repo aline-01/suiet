d8da5814f4d78683869f66f9053af037e501eb7c
alien-01 <alien-01@kali.hacker>
2022-11-24 04:14:49 -0500

    add the html pages

---
+++ includes/css_js/advertising.js
@@ -0,0 +1,10 @@
+const FORM = document.querySelector(".form-send")
+const CITY_F = document.querySelector(".city_f")
+
+console.log(CITY_F)
+
+FORM.addEventListener("submit",(form_submited)=>{
+    
+},false)
+
+
